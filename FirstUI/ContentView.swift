e52cc1a3bfecc3440dcb5319a747eb6414e52e73
den0206 <mac.pop@mineo.jp>
2020-08-18 13:32:49 +0900

    Initial Commit

---
+++ FirstUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FirstUI
+//
+//  Created by 酒井ゆうき on 2020/08/18.
+//  Copyright © 2020 Yuuki sakai. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
