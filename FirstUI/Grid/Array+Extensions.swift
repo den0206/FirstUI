378796bdd8b4c32e3a8b54e96d6f748351e7e827
den0206 <mac.pop@mineo.jp>
2020-08-20 23:26:14 +0900

    grid

---
+++ FirstUI/Grid/Array+Extensions.swift
@@ -0,0 +1,35 @@
+//
+//  Array+Extensions.swift
+//  FirstUI
+//
+//  Created by 酒井ゆうき on 2020/08/20.
+//  Copyright © 2020 Yuuki sakai. All rights reserved.
+//
+
+import Foundation
+
+extension Array {
+    func chunks(size : Int) -> [ArraySlice<Element>] {
+        var chunks : [ArraySlice<Element>] = [ArraySlice<Element>]()
+        
+        for index in stride(from: 0, to: self.count - 1, by: size ) {
+            var chunk = ArraySlice<Element>()
+            let end = index + size
+            
+            if end >= self.count {
+                chunk = self[index..<self.count]
+            } else {
+                chunk = self[index..<end]
+            }
+            
+            chunks.append(chunk)
+            
+            if (end + 1) == self.count {
+                let remainingChunk = self[end..<self.count]
+                chunks.append(remainingChunk)
+            }
+        }
+        
+        return chunks
+    }
+}
---
+++ FirstUI/Grid/GridView.swift
@@ -0,0 +1,49 @@
+//
+//  GridView.swift
+//  FirstUI
+//
+//  Created by 酒井ゆうき on 2020/08/20.
+//  Copyright © 2020 Yuuki sakai. All rights reserved.
+//
+
+import SwiftUI
+
+struct GridView: View {
+    
+    let animals = ["🐈","🐆","🦌","🦒","🦏","🐄","🐀","🦩","🦜"]
+    @State private var sliderValue : CGFloat = 1
+    
+    var body: some View {
+        
+        NavigationView {
+            
+           VStack {
+                Slider(value: $sliderValue,in: 1...8,step: 1)
+            
+                Text(String(format: "%.0f", self.sliderValue))
+                    .font(.system(size: 20))
+                    .fontWeight(.bold)
+                    .padding()
+                    .background(Color.purple)
+                    .foregroundColor(Color.white)
+                    .clipShape(Circle())
+                
+            List(self.animals.chunks(size: Int(self.sliderValue)), id: \.self) { chunk in
+                    ForEach(chunk, id: \.self) { animal in
+                        Text(animal)
+                            .font(.system(size: CGFloat(300/self.sliderValue)))
+                    }
+                }
+                
+            }
+            .navigationBarTitle("Animals")
+        }
+        
+    }
+}
+
+struct GridView_Previews: PreviewProvider {
+    static var previews: some View {
+        GridView()
+    }
+}
